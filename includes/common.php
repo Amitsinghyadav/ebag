234ee5d44a40b74a837632f7fe2dce79c2a92536
amit <amithal.lko@gmail.com>
2019-09-29 06:05:11 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
 //this file for setting connection and starting session
-    $con=mysqli_connect("localhost","root","","portablebag") or die(mysqli_error($con));
+    $con=mysqli_connect("localhost","root","root","portablebag") or die(mysqli_error($con));
     session_start();
 ?>
