a69630c355ab674e94bb87c60cddc4d5af772dad
amit <amithal.lko@gmail.com>
2019-09-29 06:10:50 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
 //this file for setting connection and starting session
-    $con=mysqli_connect("51.158.118.84","root","root","portablebag") or die(mysqli_error($con));
+    $con=mysqli_connect("51.158.118.84:33060","root","root","portablebag") or die(mysqli_error($con));
     session_start();
 ?>
